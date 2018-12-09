81d00a82aec4b6570b89f2f4f44da80e22a9c79e
Daniel Guerrero <dguerrero@ideasoft.biz>
2018-12-09 20:32:11 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+// import * as d3 from "d3";
+//
+// var d3 = require("d3"),
+//     jsdom = require("jsdom");
+//
+// var document = jsdom.jsdom(),
+//     svg = d3.select(document.body).append("svg");
+
+var data = [
+    {x: 1, y: 2},
+    {x: 100, y: 200}
+]
